2d218541328e8e9b04a1907826f8db46ee631d7f
Ashley <none>
2025-06-22 17:27:39 +0100

    give myself an easier time, stage html

--- process.py
+++ process.py
@@ -59,17 +59,24 @@ def process_csv(inputCsvPath):
     ).astype(int)
 
     ## drop the original frequency columns
-    in_df = in_df.drop(["Frequency band start", "Frequency band end"], axis=1)
+    ## and replace NaN with False
+    in_df = in_df.drop(["Frequency band start", "Frequency band end"], axis=1).fillna(False)
 
     ## build up a dict
     in_df["record"] = in_df.apply(format_record, axis=1)
 
     ## combine all the dicts into one
     compiled_records = {band: list(df["record"]) for band, df in in_df.groupby("Band")}
+    
+    ## combine all the dicts, but in a different way
+    alt_compiled_records = in_df.drop("record", axis=1).to_json(orient="records", indent=4)
 
-    return compiled_records
+    return compiled_records, alt_compiled_records
 
 
 if __name__ == "__main__":
+    processed_recs, processed_recs_alt = process_csv("res/foundation_ofcom_data.csv")
     with open("res/foundation.json", "w") as outfile:
-        outfile.write(json.dumps(process_csv("res/foundation_ofcom_data.csv"), indent=4))
+        outfile.write(json.dumps(processed_recs, indent=4))
+    with open("res/foundation_alt.json", "w") as outfile:
+        outfile.write(processed_recs_alt)
